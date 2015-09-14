2a72b9a59aa143d7b952fc33cc96ff7d391aa3a3
Administrator <Administrator@2-510-10>
2015-09-14 17:42:43 +0900

    reply dto 생성끝

---
+++ GamSungSNS_4/src/main/java/dto/Reply.java
@@ -0,0 +1,67 @@
+package dto;
+
+import java.util.Date;
+
+public class Reply {
+	private int replyNo;
+	private String replyContent;
+	private Date regDate;
+	private int boardBoardNo;
+	private int usersUserNo;
+
+	public Reply(int replyNo, String replyContent, Date regDate, int boardBoardNo, int usersUserNo) {
+		super();
+		this.replyNo = replyNo;
+		this.replyContent = replyContent;
+		this.regDate = regDate;
+		this.boardBoardNo = boardBoardNo;
+		this.usersUserNo = usersUserNo;
+	}
+
+	public int getReplyNo() {
+		return replyNo;
+	}
+
+	public void setReplyNo(int replyNo) {
+		this.replyNo = replyNo;
+	}
+
+	public String getReplyContent() {
+		return replyContent;
+	}
+
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getBoardBoardNo() {
+		return boardBoardNo;
+	}
+
+	public void setBoardBoardNo(int boardBoardNo) {
+		this.boardBoardNo = boardBoardNo;
+	}
+
+	public int getUsersUserNo() {
+		return usersUserNo;
+	}
+
+	public void setUsersUserNo(int usersUserNo) {
+		this.usersUserNo = usersUserNo;
+	}
+
+	@Override
+	public String toString() {
+		return "Reply [replyNo=" + replyNo + ", replyContent=" + replyContent + ", regDate=" + regDate
+				+ ", boardBoardNo=" + boardBoardNo + ", usersUserNo=" + usersUserNo + "]";
+	}
+
+}
