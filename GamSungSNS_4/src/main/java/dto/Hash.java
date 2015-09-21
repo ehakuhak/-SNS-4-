568e92b462aa368cbf99ddcd6643be06f1b564d8
Administrator <Administrator@2-510-09>
2015-09-21 14:49:53 +0900

    q

--- GamSungSNS_4/src/main/java/dto/Hash.java
+++ GamSungSNS_4/src/main/java/dto/Hash.java
@@ -3,38 +3,17 @@ package dto;
 public class Hash {
 	private String content;
 	private int boardBoardNo;
-	
-	public Hash() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	public Hash(String content, int boardBoardNo) {
-		super();
-		this.content = content;
-		this.boardBoardNo = boardBoardNo;
-	}
-	
 	public String getContent() {
 		return content;
 	}
-	
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
 	public int getBoardBoardNo() {
 		return boardBoardNo;
 	}
-	
 	public void setBoardBoardNo(int boardBoardNo) {
 		this.boardBoardNo = boardBoardNo;
 	}
-	
-	@Override
-	public String toString() {
-		return "HasgTag [content=" + content + ", boardBoardNo=" + boardBoardNo + "]";
-	}
-	
 	
 }
