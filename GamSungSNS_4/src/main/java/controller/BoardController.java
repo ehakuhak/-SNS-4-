f43bea8280e37236d23fcfe7b55be93c2b34bc3f
Administrator <Administrator@2-510-09>
2015-10-06 12:59:25 +0900

    목록

--- GamSungSNS_4/src/main/java/controller/BoardController.java
+++ GamSungSNS_4/src/main/java/controller/BoardController.java
@@ -1,8 +1,33 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import dto.Board;
+import service.BoardService;
 
 @Controller
 public class BoardController {
+	@Autowired
+	BoardService bservice;
 	
+	@RequestMapping(value="/allBoardList", method=RequestMethod.POST, 
+			produces="application/json;charset=UTF-8")
+	public @ResponseBody String boardList(){
+		Gson gson = new Gson();
+		List<Board> list = new ArrayList<>();
+		list = bservice.AllBoardListService();
+		
+		return gson.toJson(list);
+	}
 }
